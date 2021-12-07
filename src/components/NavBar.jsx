187087adaf807bce52f37ba026772ee09116e0a2
Yuliannn94 <yuliannn94@gmail.com>
2021-12-07 17:46:33 -0500

    agregar fuente roboto,logo y favicon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import Logotipo from '../media/Logo1.png'
+
+
 
 
 const NavBar = () => {
+    const Logo = () => {
+        return (
+          <div className='App-logo'>
+            <img src={Logotipo} alt='Logo' />
+          </div>
+        );
+      };
+      
     return (
         <div>
             <ul className="NavBar GrayNavBarGDM items-center flex text-white justify-between">
-                <ol className="flex items-center m-12">
-                    <i className="App-logo fas fa-brain text-yellow-500" />
+                <ol className="flex items-center mx-12 my-0" >
+                    <Logo/>
                     Gente Prevalente
                 </ol>
 
